9ccb229f8115d111d2d4f91b674aa73562c8f330
AngainorDev <dev@angainor.com>
2019-10-10 08:46:59 +0200

    Improve html

--- doc/Readme.md
+++ doc/Readme.md
@@ -6,11 +6,37 @@ JS Port.
 
 ## Overview
 
-First attempt:  
-Since Cardano also uses ED25519 crypto and designed a proper derivation scheme, we choose to use the same implementation.
-Cardano provides libs in various languages, making compatible tools in various languages easier to develop.
+- Generate any amount of keys from a single BIP39 mnemonic  
+- Convert any seed into a BIP39 mnemonic (paperCode) and back.
+- Usual operations on keys, incl. derivation and several formats.
 
-Failure since the private key len is not the same.
+
+## Derivation mechanism
+
+BIP 39 mnemonic (12 or more words) => master seed (512 bits, 64 bytes)
+
+master seed (64 bytes) => left = seed, right = chainCode
+
+derived_seed = sha512Hmac(parent.chainCode).digest(0 + parent.seed + UINT32(index))
+
+derived_seed (64 bytes) => left = seed, right = chainCode
+
+seed (32 bytes) => privatekey (64 bytes)
+
+privatekey (64 bytes) => publickey (32  bytes)
+
+
+## Paper codes
+
+entropy (32 bytes) <=> BIP 39 mnemonic (24 words)
+
+entropy (32 bytes) <=> seed (32 bytes)
+
+
+## Tests 
+
+See test directory for API and sample use.
+
 
 ## Changelog
 
--- js/src/index.js
+++ js/src/index.js
@@ -9,17 +9,38 @@ function generate_mnemonic() {
   element.value = mnemonic
 }
 
-function getKeyRow(index, seed, address, paperCode) {
+function getKeyRow(index, seed, address, paperCode, extraClass='') {
     return  `
-        <div class="col-12 col-md-4 col-xl-6">
+        <div class="col-12">
           <form>
-            Seed ${index}<input type="TEXT" class="form-control" value="${seed}"><br />
-            Public Id ${index}<input type="TEXT" class="form-control" value="${address}"><br />
-            paperCode ${index}<input type="TEXT" class="form-control" value="${paperCode}"><br />
+            Seed ${index}: <input type="TEXT" class="form-control" value="${seed}"> Public Id ${index}: <input type="TEXT" class="form-control" value="${address}"><br />
+            paperCode ${index}: <input type="TEXT" class="form-control" value="${paperCode}"><br />
           </form>
         </div>
     `
 }
+
+
+function getKeyRow2(index, seed, address, paperCode, tHeadClass='') {
+    return  `
+        <div class="col-12">
+          <table class="table">
+          <thead class="${tHeadClass}">
+            <tr>
+              <th>Seed ${index}</th><th>Public Id ${index}</th>
+            </tr>
+          </thead>
+            <tr>
+              <td>${seed}</td><td>${address}</td>
+            </tr>
+            <tr>
+                <td colspan="2">paperCode ${index}: <input type="TEXT" class="form-control" value="${paperCode}"></td>
+            </tr>
+          </table>
+        </div>
+    `
+}
+
 
 function generate_addresses() {
     const mnemonic = document.querySelector("#BIP39-input").value.trim()
@@ -27,9 +48,11 @@ function generate_addresses() {
     const count = parseInt(document.querySelector("#BIP39-count").value, 10)
     const wrapper = document.querySelector("#addresses")
     let content = ''
+    let extraClass=''
     for (i=1; i<=count; i++) {
         derived = MasterKey.derive(i)
-        content += getKeyRow(i, derived.toSeedHexWithDashes(), derived.toPubKeyHexWithDashes() , derived.toPaperCode())
+        content += getKeyRow2(i, derived.toSeedHexWithDashes(), derived.toPubKeyHexWithDashes() , derived.toPaperCode(), extraClass)
+        if (extraClass =='') {extraClass = 'thead-light'} else {extraClass = ''}
     }
     wrapper.innerHTML = content
 }
